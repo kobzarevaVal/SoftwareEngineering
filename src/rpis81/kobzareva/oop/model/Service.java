ff2d261478820fed611fe3b06285a0e65c6756ae
KobzarevaV <kobzareva2000@mail.ru>
2020-04-29 09:31:10 +0300

    Lab3 and corrected Lab2

--- src/rpis81/kobzareva/oop/model/Service.java
+++ src/rpis81/kobzareva/oop/model/Service.java
@@ -4,10 +4,15 @@ public final class Service {
     final private double cost;
     final private String name;
     final private ServiceTypes type;
+    final private String  DEFAULT_NAME = "интернет 100 мб\\сек";
+    final private double  DEFAULT_COST = 300;
+    final private ServiceTypes  DEFAULT_TYPE = ServiceTypes.INTERNET;
+
+
     public Service(){
-        this.name = "интернет 100 мб\\сек";
-        this.cost = 300;
-        this.type = ServiceTypes.INTERNET;
+        this.name = DEFAULT_NAME;
+        this.cost = DEFAULT_COST;
+        this.type = DEFAULT_TYPE;
     }
     public Service(String name, double cost, ServiceTypes type){
         this.cost = cost;
